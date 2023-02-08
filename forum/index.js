0792dae393cb5cb337c7e391bd07b92afabe6275
jean <jean@rehman.net>
2023-02-07 18:23:47 -0800

    .env file

--- forum/index.js
+++ forum/index.js
@@ -17,21 +17,6 @@ app.use(cors());
 
 app.use('/forum', forumRouter);
 
-//const { application } = require("express");
-//const { default: mongoose } = require("mongoose");
-
-// old version
-    const CONNECTION_URL = 'mongodb+srv://jeanr:Cheese1818!@cluster0.olsuuej.mongodb.net/?retryWrites=true&w=majority'; 
-    const PORT = process.env.PORT || 5000;
-
-    mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => app.listen(PORT, () => console.log('Server running on port: http://localhost:${PORT}')))
-        .catch(() =>(error) => console.log('${error} did not connect'));
-
-    //mongoose.set('useFindAndModify', false);
-
-
-/*
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT;
 
@@ -40,4 +25,4 @@ mongoose.set('strictQuery', true);
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Forum server running on port: ${PORT}`)))
     .catch(() => (error) => console.log(`${error} did not connect`));
-*/
+
