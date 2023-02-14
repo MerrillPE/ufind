f040a84d93b3dfe1b461de327d12d25f7313aa89
merrillpe <merrillpe@gmail.com>
2023-02-13 21:55:35 -0800

    :construction: updating google oauth client id

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,10 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import SignIn from './components/Auth/SignIn';
 import SignUp from './components/Auth/SignUp';
 
-const OAUTH_ID = process.env.REACT_APP_OAUTH;
+//const OAUTH_ID = process.env.REACT_APP_OAUTH;
 
 const App = () => (
-  <GoogleOAuthProvider clientId={OAUTH_ID} >
+  <GoogleOAuthProvider clientId='1040116133990-4mfe5j4sl187kltqlvctjcug6h6p8act.apps.googleusercontent.com' >
     <BrowserRouter>
       <Container maxWidth='lg'>
         <Routes>
