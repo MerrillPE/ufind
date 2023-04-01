39d0c34ca666511fbdaf4ccba3669fdca9791192
merrillpe <merrillpe@gmail.com>
2023-03-31 22:47:37 -0700

    :lipstick: better client validation for creating post

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,16 +1,17 @@
 import React, { useState, useRef } from 'react';
-import { Container, CssBaseline, Box, Typography, TextField, Button, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
+import { Container, CssBaseline, Box, Typography, TextField, Button, Select, MenuItem, FormControl, InputLabel, Avatar } from '@mui/material';
 import { useDispatch } from 'react-redux';
 import FileBase from 'react-file-base64';
 import { useLoadScript, Autocomplete } from '@react-google-maps/api';
 import { useNavigate } from 'react-router-dom';
+import ImageSearchOutlinedIcon from '@mui/icons-material/ImageSearchOutlined';
 
 //import { signup } from '../../actions/auth';
 import { createPost } from '../../actions/forum';
 
 
 const initialForm = { title: '', description: '', location: '', username: '', userID: '', image: '', category: '' };
-const categories = ['Pets', 'Electronics', 'Bikes/Scooters', 'Jewelry', 'Clothing', 'Wallets/Purses/Bags', 'Miscellaneous'];
+const categories = ['Pets', 'Electronics', 'Bikes and Scooters', 'Jewelry', 'Clothing', 'Wallets, Purses, and Bags', 'Miscellaneous'];
 
 // TODO: Validate file extension is either jpg or png
 const PostForm = () => {
@@ -36,6 +37,12 @@ const PostForm = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
+        const regex = /^data:image\/(jpeg|png);base64,/;
+        if (!formData.image.match(regex)) {
+            alert('File must be a jpg or png image');
+            return;
+        }
+
         // Set username for post from local storage profile
         let updatedForm;
         if (user?.username) {
@@ -59,10 +66,22 @@ const PostForm = () => {
         dispatch(createPost(submitData)).then(() => navigate('/'));
     };
 
+    const handlePlaceChanged = () => {
+        const place = locationRef.current.value;
+        setFormData({ ...formData, location: place });
+    };
+
     // update form data as user types
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
+
+    const isEmpty =
+        !formData.title ||
+        !formData.description ||
+        !formData.category ||
+        !formData.location ||
+        formData.image === '';
 
     if (!isLoaded) return (<div>Loading</div>); // avoids error if page loads before map api is loaded
 
@@ -102,7 +121,7 @@ const PostForm = () => {
                         autoComplete='description'
                         onChange={handleChange}
                     />
-                    <Autocomplete>
+                    <Autocomplete onPlaceChanged={handlePlaceChanged}>
                         <TextField
                             margin='normal'
                             required
@@ -116,7 +135,7 @@ const PostForm = () => {
                         />
                     </Autocomplete>
                     <FormControl variant="outlined" fullWidth>
-                        <InputLabel id="category-label">Category</InputLabel>
+                        <InputLabel id="category-label" required>Category</InputLabel>
                         <Select value={formData.category} name='category' label="Category" labelId="category-label" onChange={handleChange} required fullWidth>
                             {categories.map((category) => (
                                 <MenuItem key={category} value={category}>{category}</MenuItem>
@@ -129,6 +148,9 @@ const PostForm = () => {
                             multiple={false}
                             onDone={({ base64 }) => setFormData({ ...formData, image: base64 })}
                         />
+                        <Avatar src={formData.image} sx={{ m: 2, width: 150, height: 150 }} variant='square'>
+                            <ImageSearchOutlinedIcon />
+                        </Avatar>
                     </div>
                     {user && (
                         <Button
@@ -136,6 +158,7 @@ const PostForm = () => {
                             fullWidth
                             variant='contained'
                             sx={{ mt: 3, mb: 2 }}
+                            disabled={isEmpty}
                         >
                             Create Post
                         </Button>
